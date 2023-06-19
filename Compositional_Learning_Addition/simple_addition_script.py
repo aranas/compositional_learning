22f2788bdba482d34c87bfb140a542de94c7d06c
Sophie Arana <arana.sophie@gmail.com>
2023-06-19 09:55:48 +0100

    separate plotting from model training

--- Compositional_Learning_Addition/simple_addition_script.py
+++ Compositional_Learning_Addition/simple_addition_script.py
@@ -14,26 +14,6 @@ from tqdm import tqdm
 from cryptic_rnn import *
 from run_models_functions import *
 
-def plot_predcorr(config, model,test_data, model_config,title):
-    plt.figure()
-    r2, _, _ = predcorr(config, model, test_data, model_config['hidden_size'], plot_corr =True)
-    plt.title(title + '; r^2 = '+ str(round(r2, 3)))
-    plt.savefig('figures/predcorr_'+title+'.png')
-
-def plot_loss(data, title='',  colors=['green', 'yellow', 'red']):
-
-    fig, ax = plt.subplots()
-    for i, level in enumerate(data.coords['loss_type'].values):
-        level_data = data.sel(loss_type=level)
-        mean = level_data.mean(dim='sim')
-        std = level_data.std(dim='sim')
-        mean.plot.line(ax=ax, label=level, color = colors[i])
-        ax.fill_between(mean['epoch'], mean-std, mean+std, alpha=0.3,  facecolor = colors[i])
-    plt.legend()
-    
-    fig.suptitle(title, fontsize=10)
-    plt.savefig('figures/loss_'+title+'.png')
-
 def set_device():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     if device != "cuda":
@@ -53,7 +33,7 @@ def main():
     ## Generate input
     num_classes = 22
     num_inputs  = 4
-    batchsize   = 1
+    batchsize   = 2
 
     ## Initiate model
     # model parameters
@@ -61,7 +41,7 @@ def main():
         'input_size': num_classes,
         'output_size': 1,
         'num_layers': 1,
-        'hidden_size': 2,
+        'hidden_size': 1,
         'xavier_gain': 0.0001,
     }
     # Run model in parallel
@@ -69,9 +49,10 @@ def main():
     config_train = {
         'batchsize': batchsize,
         'learningRate': 0.005,
-        'epochs': 1000,
-        'num_sims': 200,
+        'epochs': 250,
+        'num_sims': 100,
         'n_train_seq': 2,
+        'valsorted': True,
     }
     random.seed(1234)
     random_seeds = random.sample(
@@ -99,6 +80,7 @@ def main():
 
     print('balanced loss', res['loss_b'][-1,:].mean())
     print('primitives loss', res['loss_p'][-1,:].mean())
+    
 
     keys_model_param = ['best_mod_p', 'best_mod_b', 'final_mod_b', 'final_mod_p', 'init_mod_b', 'init_mod_p', 'cue_dict','test', 'train_b','train_p']
     keys_loss = ['loss_b', 'loss_p', 'train_loss_b', 'train_loss_p', 'test_loss_b', 'test_loss_p']
@@ -112,20 +94,16 @@ def main():
         coords={'loss_type': list(d_losses.keys())},
     )
 
-    ##Plot loss
-    plot_loss(data.sel(loss_type=data['loss_type'].str.endswith('b')), colors = ['green', 'orange', 'red'], title = 'balanced -no primitives')
-    plot_loss(data.sel(loss_type=data['loss_type'].str.endswith('p')), colors = ['green', 'orange', 'red'], title = 'with primitives')
-
-    plot_predcorr(config_model ,d_models['final_mod_b'], d_models['test'], config_model, title = 'final: balanced -no primitives')
-    plot_predcorr(config_model, d_models['final_mod_p'], d_models['test'], config_model, title = 'final: with primitives')
-
-    plot_predcorr(config_model ,d_models['best_mod_b'], d_models['test'], config_model, title = 'best: balanced -no primitives')
-    plot_predcorr(config_model, d_models['best_mod_p'], d_models['test'], config_model, title = 'best: with primitives')
-
     ## Save models & loss
-    torch.save(d_models, f"results/{config_train['n_train_seq']}seqs_{config_model['hidden_size']}_{config_train['epochs']}_modelonly.pt")
-    with open(f"results/{config_train['n_train_seq']}seqs_{config_model['hidden_size']}_{config_train['epochs']}_losses.pkl", 'wb') as f:
+    if config_train['valsorted']==True:
+        affix = '_trainsorted'
+    else:
+        affix = ''
+    torch.save(d_models, f"results/{config_train['n_train_seq']}seqs_{config_model['hidden_size']}_{config_train['epochs']}{affix}_batchsize{config_train['batchsize']}_modelonly.pt")
+    with open(f"results/{config_train['n_train_seq']}seqs_{config_model['hidden_size']}_{config_train['epochs']}{affix}_batchsize{config_train['batchsize']}_losses.pkl", 'wb') as f:
         pickle.dump(data, f)
 if __name__ == "__main__":
     main()
 
+
+# %%
